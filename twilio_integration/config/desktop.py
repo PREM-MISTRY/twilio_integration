8b88f03fc493789dcce84fed30bd41ba38acfb76
Prem Mistry <frappe@PREM>
2025-07-10 00:14:21 +0530

    Fix: Add desktop.py to define module

---
+++ twilio_integration/config/desktop.py
@@ -0,0 +1,12 @@
+from frappe import _
+
+def get_data():
+    return [
+        {
+            "module_name": "Twilio Integration",
+            "color": "grey",
+            "icon": "octicon octicon-plug",
+            "type": "module",
+            "label": _("Twilio Integration")
+        }
+    ]
